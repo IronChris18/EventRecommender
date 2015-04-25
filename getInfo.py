9067dd87be8f437bf28b8b5943b4c19a66ec0c73
Christopher Carberry <carberr2@illinois.edu>
2015-04-25 18:05:46 -0500

    beef up getInfo

--- getInfo.py
+++ getInfo.py
@@ -1,29 +1,40 @@
+"""
+	Run this python script within child_pages to avoid
+	creating 619 new files in the parent directory
+"""
+
 import os, subprocess
 
 #get parent names
-os.system('phantomjs getLinkNames.js names.txt')
+os.system('phantomjs ../getLinkNames.js ../names.txt')
 #get parent links
-os.system('phantomjs getParent.js links.txt')
+os.system('phantomjs ../getParent.js ../links.txt')
+#replace spaces with underscores in calendar names
+os.system('sed -i s/\ /\_/g ../names.txt')
+os.system('sed -i s/\,//g ../names.txt')
 
-#read output file to get links
-##open each link and retrieve html page
-file = open('links.txt', "r")
-name = open('names.txt', "r")
-#for(i=0; i<620; i++):
-for i in range(0, 620):
+#read output file to get links and link names
+file = open('../links.txt', "r")
+name = open('../names.txt', "r")
+
+""" open each child page and get html info"""
+for j in range(0, 5):
+	garbage = name.readline()	# name child pages something useful
+for i in range(0, 624):
 	if(i == 0):
 		garbage = file.readline()
 		continue
 	
-	url = file.readline()
-	#print "page url: " + url
-	page = 'page_'+str(i)+'.txt'
+	url = file.readline()	#get child_page url
+	#page = 'page_'+str(i)+'.txt'	#name child pages numerically
+	page = name.readline()	#get calendar page name
 	#print url
-	temp = str(url[:-1])
-	subprocess.call(["phantomjs","getHtml.js",temp,page]) #no html data
+	temp = str(url[:-1])	#remove newline character on url
+	page = str(page[:-1])+".txt"	#same for page names
+	subprocess.call(["phantomjs","../getHtml.js",temp,page]) #no html data
 	#os.system("wget "+temp)	#get html data
 	print 'Page: '+str(i)+'\n'
 
 file.close()
 
-os.system('mv *.txt child_pages')
+#os.system('mv *.txt child_pages')
